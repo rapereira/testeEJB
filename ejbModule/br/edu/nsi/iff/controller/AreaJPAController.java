350fda1645b04c1a90e401594b2ba688a61efc71
rafael <alvespereira.rap@gmail.com>
2011-09-06 18:41:52 -0300

    Alterado AreaJPAController

--- ejbModule/br/edu/nsi/iff/controller/AreaJPAController.java
+++ ejbModule/br/edu/nsi/iff/controller/AreaJPAController.java
@@ -1,5 +1,22 @@
 package br.edu.nsi.iff.controller;
 
-public class AreaJPAController {
+import br.edu.nsi.iff.Area;
+import br.edu.nsi.iff.Planta;
+
+
+public class AreaJPAController extends AbstractPersistence{
+
+	public void insert(Planta planta, String uid, String descricao, String tag) {
+		
+		Area Area = new Area();
+		
+		Area.setPlanta(planta);
+		Area.setIdarea(uid);
+		Area.setDescricao(descricao);
+		Area.setTagarea(tag);
+		
+		super.insert(Area);
+	}
+	
 
 }
