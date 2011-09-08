4c92d959ef5a64fd7fd550db54816733e30b0306
rafael <alvespereira.rap@gmail.com>
2011-09-08 18:19:20 -0300

    Alterados arquivos TestePlanta e TesteArea, adicionados testes de update, delete e list.

--- ejbModule/br/edu/nsi/iff/teste/TesteArea.java
+++ ejbModule/br/edu/nsi/iff/teste/TesteArea.java
@@ -2,25 +2,61 @@ package br.edu.nsi.iff.teste;
 
 import java.util.UUID;
 
+import br.edu.nsi.iff.Area;
 import br.edu.nsi.iff.controller.AreaJPAController;
 import br.edu.nsi.iff.controller.PlantaJPAController;
 
+@SuppressWarnings("unused")
 public class TesteArea {
 
 	public static void main(String[] args){
-		
-		UUID uuid = UUID.randomUUID();
-		String uid = uuid.toString();
-        String tag = "testeinsert";
-        String descricao = "testeinsert";
-        PlantaJPAController instancePlanta = new PlantaJPAController();
-        
-        
-        instancePlanta.insert(uid, tag, descricao);
-        System.out.println("insert");
-        
-        
-        AreaJPAController instanceArea = new AreaJPAController();
-        instanceArea.insert(instancePlanta.find(uid), uid, descricao, tag);
+
+		TesteArea.list();
 	}
+	
+	public static void insert(){
+//		UUID uuid = UUID.randomUUID();
+//		String uid = uuid.toString();
+		String uid = "3";
+		String tag = "testeinsert";
+        String descricao = "testeinsert";
+        
+        PlantaJPAController instancePlanta = new PlantaJPAController();
+        AreaJPAController instanceArea = new AreaJPAController();
+        
+        instanceArea.insert(instancePlanta.find("2"), uid, descricao, tag);
+        System.out.println("insert");
+	}
+	
+	public static void update(){
+		String uid = "3";
+		String tag = "testeupdate";
+		String descricao = "testeupdate";
+		
+		PlantaJPAController instancePlanta = new PlantaJPAController();
+		AreaJPAController instanceArea = new AreaJPAController();
+		
+		instanceArea.update(instancePlanta.find("2"), uid, descricao, tag);
+		System.out.println("update");
+	}
+	
+	public static void delete(){
+		String uid = "3";
+		
+		AreaJPAController instanceArea = new AreaJPAController();
+		
+		instanceArea.delete(uid);
+		System.out.println("delete");
+	}
+	
+	public static void list(){
+		AreaJPAController instanceArea = new AreaJPAController();
+		
+		for (Area area : instanceArea.findAll()){
+			
+			System.out.println("\nArea: " + area.getIdarea());
+			System.out.println("TAG: " + area.getTagarea() + "\n");
+		}
+	}
+	
 }
--- ejbModule/br/edu/nsi/iff/teste/TestePlanta.java
+++ ejbModule/br/edu/nsi/iff/teste/TestePlanta.java
@@ -5,22 +5,55 @@ import java.util.UUID;
 import br.edu.nsi.iff.Planta;
 import br.edu.nsi.iff.controller.PlantaJPAController;
 
+@SuppressWarnings("unused")
 public class TestePlanta {
 
 	public static void main(String[] args) {
-	     
-			UUID uuid = UUID.randomUUID();
-			String uid = uuid.toString();
-	        String tag = "testeinsert";
-	        String descricao = "testeinsert";
-	        PlantaJPAController instance = new PlantaJPAController();
-	        instance.insert(uid, tag, descricao);
-	        System.out.println("insert");
-	        
-	        for (Planta planta : instance.findAll()) {
-	        	
-	        	System.out.println("Planta: "+planta.getIdplanta());
-				
-			}
-	    }
+
+		TestePlanta.list();
+	}
+
+	public static void insert(){
+		//		UUID uuid = UUID.randomUUID();
+		//		String uid = uuid.toString();
+
+		String uid = "3";
+
+		String tag = "testeinsert";
+		String descricao = "testeinsert";
+
+		PlantaJPAController instancePlanta = new PlantaJPAController();
+
+		instancePlanta.insert(uid, tag, descricao);
+		System.out.println("insert");
+	}
+
+	public static void update(){
+		String uid = "3";
+		String tag = "testeupdate";
+		String descricao = "testeupdate";
+
+		PlantaJPAController instancePlanta = new PlantaJPAController();
+
+		instancePlanta.update(uid, tag, descricao);
+		System.out.println("update");
+	}
+
+	public static void delete(){
+		String uid = "3";
+
+		PlantaJPAController instancePlanta = new PlantaJPAController();
+
+		instancePlanta.delete(uid);
+		System.out.println("delete");
+	}
+
+	public static void list(){
+		PlantaJPAController instancePlanta = new PlantaJPAController();
+
+		for (Planta planta : instancePlanta.findAll()){
+			System.out.println("\nPlanta: " + planta.getIdplanta());
+			System.out.println("TAG: " + planta.getTagplanta() + "\n");
+		}
+	}
 }
