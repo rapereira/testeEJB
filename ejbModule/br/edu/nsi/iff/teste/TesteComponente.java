27d4fb516b047baf056c2542c676658050e4e4b1
rafael <alvespereira.rap@gmail.com>
2011-09-08 18:19:56 -0300

    Adicionados arquivos TesteComponente e TesteEquipamento com funções de insert, update, delete e list funcionando.

---
+++ ejbModule/br/edu/nsi/iff/teste/TesteComponente.java
@@ -0,0 +1,58 @@
+package br.edu.nsi.iff.teste;
+
+import br.edu.nsi.iff.Componente;
+import br.edu.nsi.iff.controller.ComponenteJPAController;
+import br.edu.nsi.iff.controller.EquipamentoJPAController;
+
+public class TesteComponente {
+
+	public static void main(String[] args){
+		
+		TesteComponente.list();
+	}
+	
+	public static void insert(){
+//		UUID uuid = UUID.randomUUID();
+//		String uid = uuid.toString();
+		String uid = "3";
+		String tag = "testeinsert";
+        String descricao = "testeinsert";
+        
+        EquipamentoJPAController instanceEquipamento = new EquipamentoJPAController();
+        ComponenteJPAController instanceComponente = new ComponenteJPAController();
+        
+        instanceComponente.insert(instanceEquipamento.find("2"), uid, descricao, tag);
+        System.out.println("insert");
+	}
+	
+	public static void update(){
+		String uid = "2";
+		String tag = "testeupdate";
+		String descricao = "testeupdate";
+		
+		EquipamentoJPAController instanceEquipamento = new EquipamentoJPAController();
+		ComponenteJPAController instanceComponente = new ComponenteJPAController();
+		
+		instanceComponente.update(instanceEquipamento.find("2"), uid, descricao, tag);
+		System.out.println("update");
+	}
+	
+	public static void delete(){
+		String uid = "3";
+		
+		ComponenteJPAController instanceComponente = new ComponenteJPAController();
+		
+		instanceComponente.delete(uid);
+		System.out.println("delete");
+	}
+	
+	public static void list(){
+		ComponenteJPAController instanceComponente = new ComponenteJPAController();
+		
+		for (Componente componente : instanceComponente.findAll()){
+			
+			System.out.println("\nComponente: " + componente.getIdcomponente());
+			System.out.println("TAG: " + componente.getTagcomponente() + "\n");
+		}
+	}
+}
---
+++ ejbModule/br/edu/nsi/iff/teste/TesteEquipamento.java
@@ -0,0 +1,64 @@
+package br.edu.nsi.iff.teste;
+
+import br.edu.nsi.iff.Equipamento;
+import br.edu.nsi.iff.controller.AreaJPAController;
+import br.edu.nsi.iff.controller.EquipamentoJPAController;
+
+@SuppressWarnings("unused")
+public class TesteEquipamento {
+
+	public static void main(String[] args){
+		
+		TesteEquipamento.list();
+	}
+	
+	public static void insert(){
+//		UUID uuid = UUID.randomUUID();
+//		String uid = uuid.toString();
+		
+		String uid = "3";
+		String descricao = "testeinsert";
+		String fabricante = "testeinsert";
+		String tag = "testeinsert";
+		String tipo = "testeinsert";
+		
+		AreaJPAController instanceArea = new AreaJPAController();
+		EquipamentoJPAController instanceEquipamento = new EquipamentoJPAController();
+
+		instanceEquipamento.insert(instanceArea.find("2"), uid, descricao, fabricante, tag, tipo);
+		System.out.println("insert");
+	}
+	
+	public static void update(){
+		String uid = "2";
+		String descricao = "testeupdate";
+		String fabricante = "testeupdate";
+		String tag = "testeupdate";
+		String tipo = "testeupdate";
+		
+		AreaJPAController instanceArea = new AreaJPAController();
+		EquipamentoJPAController instanceEquipamento = new EquipamentoJPAController();
+		
+		instanceEquipamento.update(instanceArea.find("2"), uid, descricao, fabricante, tag, tipo);
+		System.out.println("update");
+	}
+	
+	public static void delete(){
+		String uid = "3";
+		
+		EquipamentoJPAController instanceEquipamento = new EquipamentoJPAController();
+		
+		instanceEquipamento.delete(uid);
+		System.out.println("delete");
+	}
+	
+	public static void list(){
+		EquipamentoJPAController instanceEquipamento = new EquipamentoJPAController();
+		
+		for (Equipamento equipamento : instanceEquipamento.findAll()){
+			
+			System.out.println("\nEquipamento: " + equipamento.getIdequipamento());
+			System.out.println("TAG: " + equipamento.getTagequipamento() + "\n");
+		}
+	}
+}
