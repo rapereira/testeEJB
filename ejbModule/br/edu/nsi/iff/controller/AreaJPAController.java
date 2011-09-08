9b51e9a40d99b41f44accec92c506a3cad477839
rafael <alvespereira.rap@gmail.com>
2011-09-08 18:18:28 -0300

    Alterado arquivo AreaJPAController, alterada função find.

--- ejbModule/br/edu/nsi/iff/controller/AreaJPAController.java
+++ ejbModule/br/edu/nsi/iff/controller/AreaJPAController.java
@@ -1,5 +1,7 @@
 package br.edu.nsi.iff.controller;
 
+import java.util.List;
+
 import br.edu.nsi.iff.Area;
 import br.edu.nsi.iff.Planta;
 
@@ -18,5 +20,38 @@ public class AreaJPAController extends AbstractPersistence{
 		super.insert(Area);
 	}
 	
+	public void insert(Area Area){
+		super.insert(Area);
+	}
+	
+	public void delete(String codigo){
+		super.delete(Area.class, codigo);
+	}
+	
+	public void update(Planta planta, String uid, String tag, String descricao){
+		Area Area = find(uid);
+		
+		Area.setPlanta(planta);
+		Area.setIdarea(uid);
+		Area.setTagarea(tag);
+		Area.setDescricao(descricao);
+		super.update(Area);
+	}
+	
+	public void update(Area Area){
+		super.update(Area);
+	}
+	
+	public List<Area> getLista(){
+		return super.findAll(Area.class);
+	}
+	
+	public Area find(String uid){
+		return super.find(Area.class, uid);
+	}
+	
+	public List<Area> findAll(){
+		return super.findAll(Area.class);
+	}
 
 }
